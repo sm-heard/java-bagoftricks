b7a948980b7cbb1afe8894ca1e6f4b33f20aed5b
Sami <samimheard@gmail.com>
2020-01-02 23:55:56 -0700

    Initial commit

---
+++ src/FizzBuzzTertiary.java
@@ -0,0 +1,12 @@
+public class FizzBuzzTertiary {
+
+  public static void main(String[] args) {
+
+    StringBuilder output = new StringBuilder();
+
+    for (int i = 0; i < 1000; i++) {
+
+      i % 3 == 0 ? (i % 5 == )
+    }
+  }
+}
