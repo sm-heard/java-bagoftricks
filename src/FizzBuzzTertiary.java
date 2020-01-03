bb7ddc3d0a6e6e40b6376eb0b5a3c80194370245
Sami <samimheard@gmail.com>
2020-01-03 00:24:25 -0700

    fbt finished

--- src/FizzBuzzTertiary.java
+++ src/FizzBuzzTertiary.java
@@ -2,11 +2,14 @@ public class FizzBuzzTertiary {
 
   public static void main(String[] args) {
 
-    StringBuilder output = new StringBuilder();
-
-    for (int i = 0; i < 1000; i++) {
-
-      i % 3 == 0 ? (i % 5 == )
+    for (int i = 1; i < 1000; i++) {
+      System.out.println(fizzBuzValue(i));
     }
+  }
+
+  private static String fizzBuzValue(int i){
+
+    return (i % 3 == 0) ? ((i % 5 == 0) ?  "FizzBuzz" : "Fizz") :
+        ((i % 5 == 0) ? "Buzz" : String.valueOf(i));
   }
 }
