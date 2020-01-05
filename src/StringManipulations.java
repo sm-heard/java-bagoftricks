238053193ba6a4b0fcb6d64cc2f1c4bd7d8def86
Sami <samimheard@gmail.com>
2020-01-04 23:56:42 -0700

    more string stuff

--- src/StringManipulations.java
+++ src/StringManipulations.java
@@ -10,6 +10,10 @@ public class StringManipulations {
     stringBuilder.replace(0, stringBuilder.length(), "new_string");
     //completely replaces a string, basically makes string mutable
 
+    stringBuilder.append("_part2");
+    System.out.println(stringBuilder);
+    //String Builder instead of + when large # of concats
+
   }
 
 
