06497146074320ef09daf641390e038660d3a027
Sami <samimheard@gmail.com>
2020-01-12 23:52:26 -0700

    math,prime search

---
+++ src/SimpleMath.java
@@ -0,0 +1,14 @@
+public class SimpleMath {
+
+  public static void main(String[] args) {
+
+    for (int i = 0; i < 1000; i++) {
+      int flag = 0;
+      for (int j = 0; j< i; j++) {
+
+
+      }
+    }
+  }
+
+}
